c3ebcf157facde1d4691de715b3ceda51dd5c2f6
Kevin Grandon <kevingrandon@yahoo.com>
2018-02-06 10:53:53 -0800

    Necessary changes for the next version of fusion-apollo

--- apollo-graphcool-boilerplate/src/main.js
+++ apollo-graphcool-boilerplate/src/main.js
@@ -4,6 +4,9 @@ import React from 'react';
 import App from 'fusion-apollo';
 import Router from 'fusion-plugin-react-router';
 import Styletron from 'fusion-plugin-styletron-react';
+import UniversalEvents, {
+  UniversalEventsToken,
+} from 'fusion-plugin-universal-events';
 import apolloClientFactory from 'fusion-apollo-universal-client';
 
 import root from './root.js';
@@ -14,5 +17,6 @@ config.fetch = global.fetch || window.fetch;
 export default () => {
   const app = new App(root, apolloClientFactory(config));
   app.register(Router);
+  app.register(UniversalEventsToken, UniversalEvents);
   return app;
 };
