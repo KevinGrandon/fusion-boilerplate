25d72591d74e1b3e7301d16e193ac0eb4e8dca53
Kevin Grandon <kevingrandon@yahoo.com>
2018-02-06 13:20:21 -0800

    Updates for DI

--- apollo-graphcool-boilerplate/src/main.js
+++ apollo-graphcool-boilerplate/src/main.js
@@ -1,22 +1,27 @@
 import 'isomorphic-fetch';
 
 import React from 'react';
-import App from 'fusion-apollo';
+import App, {ApolloClientToken} from 'fusion-apollo';
+import {FetchToken} from 'fusion-tokens';
 import Router from 'fusion-plugin-react-router';
 import Styletron from 'fusion-plugin-styletron-react';
 import UniversalEvents, {
   UniversalEventsToken,
 } from 'fusion-plugin-universal-events';
-import apolloClientFactory from 'fusion-apollo-universal-client';
+
+import ApolloClientPlugin, {
+  ApolloClientEndpointToken,
+} from 'fusion-apollo-universal-client';
 
 import root from './root.js';
 
 import config from '../config/config';
-config.fetch = global.fetch || window.fetch;
 
 export default () => {
-  const app = new App(root, apolloClientFactory(config));
+  const app = new App(root);
   app.register(Router);
   app.register(UniversalEventsToken, UniversalEvents);
+  app.register(ApolloClientToken, ApolloClientPlugin);
+  app.register(ApolloClientEndpointToken, config.endpoint);
   return app;
 };
