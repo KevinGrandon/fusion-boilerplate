38707628ed583e0bc4439a5d102cd74bf9e903b5
Kevin Grandon <kevingrandon@yahoo.com>
2018-06-12 11:27:46 -0700

    Add example of server-side fetch with CSRF

--- apollo-graphcool-boilerplate/src/main.js
+++ apollo-graphcool-boilerplate/src/main.js
@@ -2,16 +2,25 @@ import 'isomorphic-fetch';
 
 import React from 'react';
 import App, {ApolloClientToken} from 'fusion-apollo';
-import {FetchToken} from 'fusion-tokens';
+import {FetchToken, SessionToken} from 'fusion-tokens';
+import Session, {
+  SessionSecretToken,
+  SessionCookieNameToken,
+} from 'fusion-plugin-jwt';
 import Router from 'fusion-plugin-react-router';
 import Styletron from 'fusion-plugin-styletron-react';
 import UniversalEvents, {
   UniversalEventsToken,
 } from 'fusion-plugin-universal-events';
-
 import ApolloClientPlugin, {
   ApolloClientEndpointToken,
 } from 'fusion-apollo-universal-client';
+import CsrfProtectionPlugin, {
+  FetchForCsrfToken,
+  CsrfIgnoreRoutesToken,
+} from 'fusion-plugin-csrf-protection-react';
+
+import {createToken} from 'fusion-core';
 import unfetch from 'unfetch';
 
 import root from './root.js';
@@ -20,11 +29,21 @@ import config from '../config/config';
 
 export default () => {
   const app = new App(root);
-  __BROWSER__ && app.register(FetchToken, window.fetch);
-  __NODE__ && app.register(FetchToken, unfetch);
+
+  const FetchTokenForApolloClient = createToken('FetchTokenForApolloClient');
+  app.register(FetchTokenForApolloClient, unfetch);
+
+  app.register(FetchToken, CsrfProtectionPlugin);
+  __NODE__ && app.register(CsrfIgnoreRoutesToken, ['/_errors']);
+  __NODE__ && app.register(SessionToken, Session);
+  __NODE__ && app.register(SessionSecretToken, 'some-secret');
+  __NODE__ && app.register(SessionCookieNameToken, 'jwt-session');
+  __BROWSER__ && app.register(FetchForCsrfToken, unfetch);
   app.register(Router);
   app.register(UniversalEventsToken, UniversalEvents);
-  app.register(ApolloClientToken, ApolloClientPlugin);
+  app
+    .register(ApolloClientToken, ApolloClientPlugin)
+    .alias(FetchToken, FetchTokenForApolloClient);
   app.register(ApolloClientEndpointToken, config.endpoint);
   return app;
 };
