aca1eee51541c1e479b77353f7e969ddaf2d31f5
Kevin Grandon <kevingrandon@yahoo.com>
2018-08-31 22:25:10 -0700

    Add electron boilerplate that loads files from disk directly

---
+++ electron-boilerplate-disk/main.js
@@ -0,0 +1,59 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  // mainWindow.loadURL(`http://localhost:3000`);
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// Start Fusion.js server
+// const env =
+//   process.env.NODE_ENV === 'production' ? 'production' : 'development';
+// const entry = path.resolve(
+//   __dirname,
+//   `.fusion/dist/${env}/server/server-main.js`
+// );
+// const {start} = require(entry);
+// return start({__dirname, port: 3000});
---
+++ electron-boilerplate-disk/src/pages/page2.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Page2 = () => <div>Page 2</div>;
+export default Page2;
