b44d1d4b5074ee3b242f974d19cc9a4235769d1a
Kevin Grandon <kevingrandon@yahoo.com>
2018-09-05 15:41:58 -0700

    Update material-ui app

--- material-ui-boilerplate/src/pages/home.js
+++ material-ui-boilerplate/src/pages/home.js
@@ -2,9 +2,11 @@
 import React from 'react';
 
 import Button from '@material-ui/core/Button';
+import TopBar from './topbar';
 
 const Home = () => (
   <div>
+    <TopBar />
     <Button variant="contained" color="primary">
       Hello World
     </Button>
